478074bdba0f4194c66ba54369b2fe904192445c
Gregory Marco <gmarco@newtek.com>
2022-12-07 13:35:02 -0600

    Add syntactic sugar for applying KO bindings to jquery elements.

--- src/jquery-knockout-plugin.js
+++ src/jquery-knockout-plugin.js
@@ -5,14 +5,31 @@ if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery', 'knockout'], factory);
 } else if (typeof require === 'function' && typeof module === 'object') {
-	module.exports = factory;
+    module.exports = factory;
 } else {
     // Browser globals
     factory(jQuery, ko);
 }
 }(function ($, ko) {
-	$.fn.koApplyBindings = function(viewModel) {
+	$.fn.koBind = function(bindings) {
 		return this.each(function() {
+			let bindingValues = [];
+			for (let key in bindings) {
+				let value = bindings[key];
+				if (typeof value !== "string") {
+					this._koBinds = this._koBinds || {};
+					this._koBinds[key] = value;
+					value = "$element._koBinds." + key;
+				}
+
+				bindingValues.push(`${key}: ${value}`);
+			}
+			this.dataset.bind =  bindingValues.join(",");
+		});
+	};
+
+	$.fn.koApplyBindings = function(viewModel) {
+		return this.each(function() { 
 			ko.applyBindings(viewModel, this);
 		});
 	};
